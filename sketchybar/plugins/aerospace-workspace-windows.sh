7d3e310b24388f7d28c13eda7429f6601888d443
Duc Hoang <hoangkimduc.dev@gmail.com>
2025-05-25 04:15:04 +0700

    sketchybar

---
+++ sketchybar/plugins/aerospace-workspace-windows.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+source ~/.config/sketchybar/icon_map.sh
+
+WORKSPACE=$(aerospace list-workspaces --focused)
+sketchybar --set group.$WORKSPACE background.color=0xEB1e1e2e
+
+contains_element() {
+  local target_element="$1"
+  shift # Remove the first argument (target_element) from the list of arguments
+  local element
+  for element in "$@"; do # Loop through the remaining arguments (array elements)
+    if [[ "$element" == "$target_element" ]]; then
+      return 0 # Element found (Bash true)
+    fi
+  done
+  return 1 # Element not found (Bash false)
+}
+
+FOCUSED=$(aerospace list-windows --focused --format "%{window-id}")
+
+new_windows=()
+while IFS= read -r line; do
+  window_id="${line%% | *}"
+  app_name="${line#* | }"
+
+  icon_result=""
+  __icon_map "$app_name"
+  window="space.$WORKSPACE.$window_id"
+  new_windows+=("\"$window\"")
+
+  if [ -z "$(sketchybar --query "$window")" ]; then
+    sketchybar --add item $window left \
+      --set $window icon="$icon_result" \
+      icon.padding_right=0 \
+      label.padding_left=0 \
+      --move $window before space.$WORKSPACE
+
+    sketchybar --remove group.$WORKSPACE
+    sketchybar --add bracket group.$WORKSPACE "/space\.$WORKSPACE.*/"
+  fi
+
+  echo "$window_id $FOCUSED"
+  if [ $window_id = $FOCUSED ]; then
+    sketchybar --set $window background.color=0xfff5a623
+  else
+    sketchybar --set $window background.color=0x00000000
+  fi
+
+done < <(aerospace list-windows --workspace focused --format "%{window-id} | %{app-name}")
+
+while IFS= read -r item; do
+  if ! contains_element "$item" "${new_windows[@]}"; then
+    temp_item="${item#\"}"
+    item_surrounding_quotes_removed="${temp_item%\"}"
+    sketchybar --remove $item_surrounding_quotes_removed
+  fi
+done < <(sketchybar --query bar | jq -e ".items[] | select(startswith(\"space.$WORKSPACE.\"))")
--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/env bash
 
-# make sure it's executable with:
-# chmod +x ~/.config/sketchybar/plugins/aerospace.sh
+sketchybar --query bar | jq -e ".items[] | select(. == \"space.$1\")" >/dev/null || (
+  sketchybar --add item space.$1 left \
+    --set space.$1 \
+    label.padding_left=0 \
+    background.drawing=off \
+    label="$1" \
+    click_script="aerospace workspace $1"
+  sketchybar --add item margin.$1 left \
+    --set margin.$1 icon.drawing=off label.drawing=off \
+    background.drawing=off width=10 \
+    --move margin.$1 after space.$1
 
-if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-  sketchybar --set $NAME background.color=0xEB1e1e2e
-else
-  sketchybar --set $NAME background.color=0xff9b9b9b
-fi
+  sketchybar --add bracket group.$1 "/space\.$1.*/" \
+    --set group.$1 background.color=0xff9b9b9b
+)
+
+sketchybar --set group.$2 background.color=0xff9b9b9b
+sketchybar --set "/space\.$2\..*/" background.color=0x00000000
+output=$(aerospace list-windows --workspace $2)
+[ -z "$output" ] && sketchybar --remove space.$2
+
+# sketchybar --set space.$1 background.color=0xEB1e1e2e
